7847d191444f48172c0dbfd71d61ced8811203bb
Breno Prata <brenoprata@hotmail.com>
2024-04-22 19:22:58 +0200

    Add spawn runner for threads

--- src/installer.rs
+++ src/installer.rs
@@ -1,4 +1,5 @@
-use std::{cmp::Ordering, error::Error, process::Command, thread::{self, JoinHandle}};
+use std::{error::Error, process::Command, thread::{self, JoinHandle}, time::Duration};
+use indicatif::ProgressBar;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -6,70 +7,55 @@ pub struct Data {
     library: Vec<LibraryConfig>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct LibraryConfig {
     name: String,
     install_script: String,
     allow_async: Option<bool>,
 }
 
-pub fn install(mut data: Data) -> Result<(), Box<dyn Error>> {
-    let mut thread_handles: Vec<JoinHandle<()>> = Vec::new();
+pub fn install(data: Data) -> Result<(), Box<dyn Error>> {
+    let libraries: Vec<&LibraryConfig> = data.library.iter().filter(|library| !library.allow_async.unwrap_or(false)).collect();
+    let async_libraries: Vec<&LibraryConfig> = data.library.iter().filter(|library| library.allow_async.unwrap_or(false)).collect();
+    let bar = ProgressBar::new_spinner();
+    bar.enable_steady_tick(Duration::from_millis(100));
+    bar.finish();
 
-    data.library
-        .sort_unstable_by(
-            |lib1, lib2| {
-                let lib1_allow_async = lib1.allow_async.unwrap_or(false);   
-                let lib2_allow_async = lib2.allow_async.unwrap_or(false);   
-                if !lib1_allow_async && lib2_allow_async
-                    {Ordering::Greater} 
-                else if !lib2_allow_async && lib1_allow_async
-                    {Ordering::Less}
-                else 
-                    {Ordering::Equal}
-            }
-        );
+    spawn_runner(async_libraries);
 
-    for library in data.library {
-        let allow_async = library.allow_async.unwrap_or(false);
-        if allow_async {
-            let handle = install_library_async(library);
-            thread_handles.push(handle);
-        } else {
-            install_library(library);
-        }
-    }
-
-    for handle in thread_handles {
-        handle.join().unwrap();
+    for library in libraries {
+        install_library(library.clone());
     }
 
     Ok(())
 }
 
-fn install_library_async(library: LibraryConfig) -> JoinHandle<()> {
-    thread::spawn(move || {
-        install_library(library);
-    })
+fn spawn_runner(libraries: Vec<&LibraryConfig>) {
+    let mut thread_handles: Vec<JoinHandle<()>> = Vec::new();
+    
+    for library in libraries {
+        let library_data = library.clone();
+        let handle = thread::spawn(move || {
+            install_library(library_data);
+        });
+        thread_handles.push(handle);
+    }
+
+    for handle in thread_handles {
+        handle.join().unwrap();
+    }
 }
 
+
 fn install_library(library: LibraryConfig) {
-    let error_output = "Command failed";
-    println!("\n\x1b[33m=======================================\n");
-    println!("\x1b[0mInstalling: \x1b[32m{}", library.name);
-
     for command in library.install_script.split("&&") {
-        println!("\x1b[0mRunning: \x1b[32m{}", command.trim());
-
-        let output = runner(command).unwrap_or_else(|error| {
+        runner(command).unwrap_or_else(|error| {
             let error_message = String::from(
                 format!("{}: {}\n{}", library.name, library.install_script, error)
                 );
             eprintln!("\x1b[31m{error_message}");
-            error_output.to_string()
+            "Command failed".to_string()
         });
-
-        println!("\x1b[0m{output}\n");
     }
 }
 
